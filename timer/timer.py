9d4b1621c66afacf43762c01f3f16f54ec199932
seyedmm <seyedmm1386@chmail.ir>
2021-03-30 13:51:25 +0430

    Edit files and add dismis

--- timer/timer.py
+++ timer/timer.py
@@ -1,9 +1,10 @@
 from winsound import Beep
 from time import localtime, strftime, sleep
+import keyboard
 
-alarm_time=input("What time does the alarm sound?(Enter 05 13 or 00 15 in 24-hour format):")
+alarm_time=input("What time does the alarm sound?(Enter it in 24-hour format like 05:13 or 00:15):")
 text = input("If your alarm has text, enter it:")
-pattern = "%H %M"
+pattern = "%H:%M"
 
 
 while True:
@@ -11,7 +12,9 @@ while True:
     if now==alarm_time:
         print(text)
         print("Your alarm time has come. It is now "+alarm_time)
-        for i in range(3):
+        while True:
             Beep(1000,500)
+            if keyboard.is_pressed("Esc"):
+                break
         break
     sleep(15)
